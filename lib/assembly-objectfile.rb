0154cfceca07b970fc5cfd5fb2da1946562affb8
Peter Mangiafico <peter@mangiafico.org>
2012-04-12 17:38:15 -0700

    add new config parameters that are used by consumers of the gem

--- lib/assembly-objectfile.rb
+++ lib/assembly-objectfile.rb
@@ -6,40 +6,19 @@ module Assembly
   # if input image is not one of these mime types, an error will be raised
   VALID_IMAGE_MIMETYPES=["image/jpeg","image/tiff"]
   
-  # Defines actual file MIME type and the corresponding "format" attribute in the content metadata XML file.
-  # See https://consul.stanford.edu/display/chimera/DOR+file+types+and+attribute+values.
-  FORMATS = {
-    'image/jp2'                => 'JPEG2000',
-    'image/jpeg'               => 'JPEG',
-    'image/tiff'               => 'TIFF',
-    'image/tiff-fx'            => 'TIFF',
-    'image/ief'                => 'TIFF',
-    'image/gif'                => 'GIF',
-    'text/plain'               => 'TEXT',
-    'text/html'                => 'HTML',
-    'text/csv'                 => 'CSV',
-    'audio/x-aiff'             => 'AIFF',
-    'audio/aiff'               => 'AIFF',
-    'audio/x-mpeg'             => 'MP3',
-    'audio/mpeg'               => 'MP3',
-    'audio/x-wave'             => 'WAV',
-    'audio/wave'               => 'WAV',
-    'audio/x-wav'              => 'WAV',
-    'audio/wav'                => 'WAV',
-    'video/mpeg'               => 'MP2',
-    'video/quicktime'          => 'QUICKTIME',
-    'video/x-msvideo'          => 'AVI',
-    'application/pdf'          => 'PDF',
-    'application/zip'          => 'ZIP',
-    'application/xml'          => 'XML',
-    'application/tei+xml'      => 'TEI',
-    'application/msword'       => 'WORD',
-    'application/wordperfect'  => 'WPD',
-    'application/mspowerpoint' => 'PPT',
-    'application/msexcel'      => 'XLS',
-    'application/x-tar'        => 'TAR',
-    'application/octet-stream' => 'BINARY',
-  }
+  # default publish/preserve/shelve attributes used in content metadata
+  FILE_ATTRIBUTES=Hash.new
+  # if no mimetype specific attributes are specified for a given file, define some defaults, and override for specific mimetypes below
+  FILE_ATTRIBUTES['default']={:preserve=>'yes',:shelve=>'no',:publish=>'no'}
+  FILE_ATTRIBUTES['image/tif']={:preserve=>'yes',:shelve=>'no',:publish=>'no'}
+  FILE_ATTRIBUTES['image/jp2']={:preserve=>'no',:shelve=>'yes',:publish=>'yes'}
+  FILE_ATTRIBUTES['image/jpeg']={:preserve=>'no',:shelve=>'yes',:publish=>'yes'}
+  FILE_ATTRIBUTES['audio/wav']={:preserve=>'yes',:shelve=>'no',:publish=>'no'}
+  FILE_ATTRIBUTES['audio/x-wav']={:preserve=>'yes',:shelve=>'no',:publish=>'no'}
+  FILE_ATTRIBUTES['audio/mp3']={:preserve=>'no',:shelve=>'yes',:publish=>'yes'}
+  FILE_ATTRIBUTES['audio/mpeg']={:preserve=>'no',:shelve=>'yes',:publish=>'yes'}
+  FILE_ATTRIBUTES['application/pdf']={:preserve=>'yes',:shelve=>'yes',:publish=>'yes'}
+  FILE_ATTRIBUTES['plain/text']={:preserve=>'yes',:shelve=>'yes',:publish=>'yes'}
   
 end
 
--- lib/assembly-objectfile/version.rb
+++ lib/assembly-objectfile/version.rb
@@ -1,3 +1,3 @@
 module Assembly
-  VERSION = "1.0.3"
+  VERSION = "1.0.4"
 end
